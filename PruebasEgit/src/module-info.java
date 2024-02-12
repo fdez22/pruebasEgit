1b47addccc3efa964d26598f889f343e9ce7b060
ignacio <ignacio@gmail.com>
2024-02-12 09:17:47 +0100

    Commit1

---
+++ PruebasEgit/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PruebasEgit {
+}
---
+++ PruebasEgit/src/pruebasEgit/PruebasEgit.java
@@ -0,0 +1,9 @@
+package pruebasEgit;
+
+public class PruebasEgit {
+
+	public static void main(String[] args) {
+		System.out.println("PruebasEgit");
+	}
+
+}
